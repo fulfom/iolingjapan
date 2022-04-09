fa3f0d2d3c508bff34c76a1c407e5662a59172de
fulfom <la.fulfom@gmail.com>
2022-04-09 20:46:54 +0900

    APLO2022 競技者ページ公開

---
+++ assets/js/contest/adminAplo2022.js
@@ -0,0 +1,255 @@
+const namelist = document.getElementById("namelist");
+const portal = document.getElementById("portal");
+
+var db = new Dexie("imgDB");
+db.version(1).stores({
+    img: '&key,url,blob'
+});
+
+//
+// Put some data into it
+//
+//   db.img.put({name: "Nicolas", shoeSize: 8}).then (function(){
+//       //
+//       // Then when data is stored, read from it
+//       //
+//       return db.id.get('Nicolas');
+//   }).then(function (friend) {
+//       //
+//       // Display the result
+//       //
+//       alert ("Nicolas has shoe size " + friend.shoeSize);
+//   }).catch(function(error) {
+//      //
+//      // Finally don't forget to catch any error
+//      // that could have happened anywhere in the
+//      // code blocks above.
+//      //
+//      alert ("Ooops: " + error);
+//   });
+
+let cachedLink = [];
+let handlers = [];
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+            const sn = await firebase.database().ref("/adminValue/" + user.uid).once("value");
+            const adminValue = sn.val();
+            if (adminValue && adminValue.conts) {
+                Object.entries(adminValue.conts).forEach((e) => {
+                    const uid = e[0];
+                    const v = e[1];
+                    if (v) {
+                        let li = document.createElement("li");
+                        li.classList.add("list-group-item");
+                        li.innerHTML = `<a href="#${uid}">${v.name}</a>`;
+                        namelist.appendChild(li);
+                        let div = document.createElement("div");
+                        div.id = uid;
+                        div.innerHTML = `<h5>${v.name} / ${v.num}</h5>`;
+                        [{
+                            key: "blank",
+                            name: "白紙"
+                        },
+                        {
+                            key: "FILE_PRE1",
+                            name: "身分証"
+                        },
+                        {
+                            key: "FILE_PRE2",
+                            name: "上シール"
+                        },
+                        {
+                            key: "FILE_PRE3",
+                            name: "下シール"
+                        },
+                        {
+                            key: "FILE_PROB1",
+                            name: "問題1"
+                        },
+                        {
+                            key: "FILE_PROB2",
+                            name: "問題2"
+                        },
+                        {
+                            key: "FILE_PROB3",
+                            name: "問題3"
+                        },
+                        {
+                            key: "FILE_PROB4",
+                            name: "問題4"
+                        },
+                        {
+                            key: "FILE_PROB5",
+                            name: "問題5"
+                        },
+                        {
+                            key: "FILE_Q",
+                            name: "アンケート"
+                        },].forEach((titles) => {
+                            div.innerHTML += `<div>
+                            <p class="d-inline-block">${titles.name}</p><button id="btn-${uid + titles.key}" class="ml-3 btn btn-primary btn-small" onclick="inputCheck(this)" data-uid="${uid}" data-key="${titles.key}">確認する</button>
+                            <div id="${uid + titles.key}" class="prob-preview">
+                            </div>`;
+                        })
+                        portal.appendChild(div);
+                        handlers[uid] = adminPortal(v, uid, div);
+                        if (v.checks) {
+                            Object.keys(v.checks).forEach((key) => {
+                                document.getElementById("btn-" + uid + key).disabled = true;
+                            })
+                        }
+                    }
+                });
+
+            }
+        }
+        else location.href = "/login/";
+    });
+});
+
+function adminPortal(cont, uid, div) {
+    const name = cont.name;
+    const num = cont.num;
+    return firebase.database().ref("/contests/aplo2022/storage/" + uid).on("value", (sn) => {
+        const val2 = sn.val();
+        if (val2) {
+            Object.keys(val2).forEach((key) => { //key == FILE_PRE1 など
+                let value = val2[key];
+                let section = document.getElementById(uid + key);
+                Object.keys(value).forEach((key2) => { //key2 == ファイルごとの key
+                    console.log(value[key2]);
+                    if (!cachedLink || !cachedLink[key] || !cachedLink[key][key2]) {
+                        addImg(value[key2], uid, key, key2, section);
+                    }
+                }, value);
+            }, val2);
+            if (cachedLink[uid]) {
+                Object.keys(cachedLink[uid]).forEach((key) => {
+                    let value = cachedLink[uid][key];
+                    Object.keys(value).forEach((key2) => {
+                        console.log(value[key2]);
+                        if (!val2 || !val2[key] || !val2[key][key2]) {
+                            const toberomoved = document.getElementById(key2);
+                            toberomoved.parentNode.removeChild(toberomoved);
+                        }
+                    }, value);
+                }, cachedLink[uid]);
+            }
+
+            cachedLink[uid] = val2;
+        }
+    });
+}
+
+async function addImg(imageUrl, uid, elemId, key2, elem) {
+    const preview = elem;
+    const div = document.createElement("div");
+    div.id = key2;
+    preview.appendChild(div); // #previewの中に追加
+    const cachedImg = await db.img.get(key2);
+    if (cachedImg) {
+        showBlob(cachedImg.blob, div, uid, elemId);
+    }
+    else {
+        if (filetype(imageUrl, "heic")) {
+            div.innerHTML = `<button class="btn btn-info btn-small" onclick="showHeic()" data-url="${imageUrl}" data-key="${key2}" data-uid="${uid}">要変換</button>`;
+        }
+        else {
+            await fetch(imageUrl)
+                .then((res) => res.blob())
+                .then((blob) => {
+                    db.img.put({ key: key2, blob: blob, url: imageUrl });
+                    showBlob(blob, div, uid, elemId);
+                })
+        }
+    }
+}
+
+function showBlob(blob, div, uid, elemId) {
+    const a = document.createElement("a");
+    const img = document.createElement("img"); // img要素を作成
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = function (evt) {
+        let srcurl = reader.result;
+        a.href = srcurl;
+        img.src = srcurl;
+        a.setAttribute("data-lightbox", uid + elemId);
+        div.appendChild(a);
+        a.appendChild(img);
+    }
+}
+
+lightbox.option({
+    "alwaysShowNavOnTouchDevices": true,
+    "disableScrolling": true
+})
+
+async function inputCheck(e) {
+    let user = firebase.auth().currentUser;
+    console.log(e)
+    if (user) {
+        const uid = e.getAttribute("data-uid");
+        const key = e.getAttribute("data-key");
+        e.disabled = true;
+        await firebase.database().ref("/adminValue/" + user.uid + "/conts/" + uid + "/checks/").update({ [key]: true });
+    }
+}
+
+function getExt(filename) {
+    let pos = filename.lastIndexOf('.');
+    if (pos === -1) return '';
+    return filename.slice(pos + 1);
+}
+
+function filetype(url, type) {
+    const reg = new RegExp(`\.${type}`, "i");
+    return url.match(reg);
+}
+
+// async function get(db, id) { // NOTE: if not found, resolves with undefined.
+//     return new Promise((resolve, reject) => {
+//       const docs = db.transaction([DOCNAME, ]).objectStore(DOCNAME);
+//       const req = docs.get(id);
+//       req.onsuccess = () => resolve(req.result);
+//       req.onerror = reject;
+//     });
+//   }
+
+function showHeic(e) {
+    var e = e || window.event;
+    var elem = e.target || e.srcElement;
+    var elemId = elem.id;
+    const imageUrl = elem.getAttribute("data-url");
+    const key = elem.getAttribute("data-key");
+    const uid = elem.getAttribute("data-uid");
+    const div = elem.parentNode;
+    const a = document.createElement("a");
+    const img = document.createElement("img"); // img要素を作成
+    fetch(imageUrl)
+        .then((res) => res.blob())
+        .then((blob) => heic2any({ blob }))
+        .then((conversionResult) => {
+            // conversionResult is a BLOB
+            // of the PNG formatted image
+            db.img.put({ key: key, blob: conversionResult, url: imageUrl });
+            let reader = new FileReader();
+            reader.readAsDataURL(conversionResult);
+            reader.onload = function (evt) {
+                elem.parentNode.removeChild(elem);
+                let srcurl = reader.result;
+                a.href = srcurl;
+                img.src = srcurl;
+                a.setAttribute("data-lightbox", uid + elemId);
+                div.appendChild(a);
+                a.appendChild(img);
+                console.log("converted" + srcurl);
+            }
+        })
+        .catch((e) => {
+            // see error handling section
+            alert("変換エラー（間を置く）");
+        });
+}
---
+++ assets/js/contest/aplo2022.js
@@ -0,0 +1,256 @@
+const app = Vue.createApp({
+    mounted() {
+        const _this = this;
+        firebase.auth().onAuthStateChanged(async (user) => {
+            if (user) {
+                const ref = firebase.database().ref("/contests/aplo2022/storage").orderByChild('uid').equalTo(user.uid)
+
+                ref.on('value', (snapshot) => {
+                    console.log('log');
+                    _this.imageItems = snapshot.val() ? (Object.entries(Object.entries(snapshot.val()).sort((a, b) => {
+                        if (a[1].for < b[1].for) {
+                            return -1
+                        }
+                        if (a[1].for > b[1].for) {
+                            return 1
+                        }
+                        return 0
+                    })).map(e => { return { index: +e[0], key: e[1][0], ...e[1][1], title: _this.formAnswers.find(f => f.id === e[1][1].for).label } })) : []
+                });
+
+                const snapshot = await firebase.database().ref("/contests/aplo2022/users/" + user.uid).once("value");
+                const val = snapshot.val();
+                if (val) {
+                    _this.edit = val;
+                    _this.submitMessage = '更新'
+                }
+                else {
+                    const badgesSnapshot = await firebase.database().ref("/badges/" + user.uid).once("value");
+                    if (badgesSnapshot.val()) {
+                        const badges = badgesSnapshot.val()
+                        if (badges.aplo2022) {
+                            const userSnapshot = await firebase.database().ref("/users/" + user.uid).once("value");
+                            _this.edit = { ...userSnapshot.val(), pa: false, preUniv: false };
+                        }
+                        else location.href = "/account/"
+                    }
+                    else {
+                        location.href = "/newuser/";
+                    }
+                }
+
+                // document.getElementById("app").classList.remove('opacity-0')
+                document.getElementById("loading").classList.add('d-none')
+            }
+            else {
+                location.href = "/login/"
+            }
+        })
+        this.submit = debounce(async () => {
+            await updateDatabasePushContToUser(this.edit, "/contests/aplo2022/").catch((err) => { alert('エラー') });
+            window.removeEventListener('beforeunload', onBeforeunloadHandler);
+            alert(`${_this.submitMessage}しました`)
+        }, 250)
+    },
+    data() {
+        return {
+            //解答提出
+            visible: false,
+            index: 0,
+            imageItems: [],
+            selected: '',
+            imageSelected: '',
+            formAnswers: [
+                {
+                    id: 'idcard',
+                    label: '身分証',
+                },
+                {
+                    id: 'prob1',
+                    label: '問題1',
+                },
+                {
+                    id: 'prob2',
+                    label: '問題2',
+                },
+                {
+                    id: 'prob3',
+                    label: '問題3',
+                },
+                {
+                    id: 'prob4',
+                    label: '問題4',
+                },
+                {
+                    id: 'prob5',
+                    label: '問題5',
+                }
+            ],
+            //個人情報
+            edit: {},
+            submit: null,
+            submitMessage: '応募',
+            form: [
+                {
+                    id: 'email',
+                    label: 'メールアドレス',
+                    type: 'email',
+                    disabled: true,
+                    help: '変更不可',
+                    required: false,
+                },
+                {
+                    id: 'name',
+                    label: '氏名（フルネーム）',
+                    help: '郵送時の宛名・賞状への記名に用います．'
+                },
+                {
+                    id: 'nameRoman',
+                    label: '氏名（ローマ字）',
+                    pattern: '^[0-9A-Za-z\\s]+$',
+                    help: '半角英数（例: Namae Myouji / MYOUJI Namae）．こちらで大文字小文字・スペースなどを調整した後，APLO国際ランキングや賞状への記名などに用います．名字名前の順番やスペルが希望通りか確認してください．'
+                },
+                {
+                    id: 'birthdate',
+                    label: '生年月日',
+                    type: 'date',
+                    min: '2002-07-25',
+                    feedback: '2002年7月25日以降の生まれである必要があります．',
+                },
+                {
+                    id: 'preUniv',
+                    label: '2022年4月10時点で私は大学教育を受けたことがありません',
+                    type: 'checkbox',
+                },
+                {
+                    id: 'schoolName',
+                    label: '2022年4月10日時点での所属学校名',
+                    help: '正式名称を略さずに記入．所属していなければ「なし」'
+                },
+                {
+                    id: 'grade',
+                    label: '2022年4月10日時点での学年',
+                    type: 'number',
+                    help: '数字．所属していなければ-1'
+                },
+                {
+                    id: 'zipcode',
+                    label: '郵便番号',
+                    pattern: '^[0-9]+$',
+                    help: 'ハイフン不要'
+                },
+                {
+                    id: 'address',
+                    label: '住所',
+                    help: '問題・賞状の送付に使用します．'
+                },
+                {
+                    id: 'publish',
+                    label: 'ウェブページへの氏名の掲載を希望しない',
+                    type: 'checkbox',
+                    required: false,
+                    help: '掲載を希望しない場合はチェックする',
+                },
+                {
+                    id: 'pa',
+                    label: 'APLO2022への参加にあたって私は保護者に有効な同意を得ています',
+                    type: 'checkbox'
+                }
+            ]
+        }
+    },
+    computed: {
+    },
+    methods: {
+        showImg(index) {
+            this.index = index
+            this.visible = true
+        },
+        handleHide() {
+            this.visible = false
+        },
+        async handleFileSelect(event) {
+            const e = event || window.event;
+            const elem = e.target || e.srcElement;
+            const files = elem.files;
+            for (let i = 0; i < files.length; i++) {
+                // previewFile(files[i], elemId);
+                let file = files[i];
+                if (file.size > 1024 * 1024 * 20) {
+                    alert('ファイルサイズが大きすぎます． 20MB以下にしてください．')
+                    elem.value = '';
+                    return;
+                }
+                // let file;
+                // if(filetype(files[i].name, "heic")){
+                //     label.innerText = "変換中"
+                //     let blob = files[i];
+                //     let result = await heic2any({blob});
+                //     file = result;
+                // }
+                // else file = files[i];
+
+                await this.uploadFile(file, elem);
+                // console.log(files[i]);　// 1つ1つのファイルデータはfiles[i]で取得できる
+            }
+            // window.removeEventListener('beforeunload', onBeforeunloadHandler);
+        },
+        // Create a root reference
+        async uploadFile(file, elem) {
+            let user = firebase.auth().currentUser;
+            if (user) {
+                const label = elem.parentNode.getElementsByTagName("label")[0];
+                const rootRef = firebase.storage().ref('contests/aplo2022/users/').child(user.uid);
+                let uploadTask = rootRef.child(elem.id + Date.now() + file.name).put(file);
+
+                uploadTask.on('state_changed', (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    elem.parentNode.getElementsByTagName("span")[0].innerText = `${progress}%完了`;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log('Upload is running');
+                            break;
+                    }
+                }, function (error) {
+                    // Handle unsuccessful uploads
+                }, function () {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    uploadTask.snapshot.ref.getDownloadURL().then(async (downloadURL) => {
+                        console.log('File available at', downloadURL);
+                        let sn = await firebase.database().ref("/contests/aplo2022/storage/").push({
+                            uid: user.uid,
+                            src: downloadURL,
+                            for: elem.id,
+                        });
+                    });
+                });
+            }
+        },
+        async removeImg(key, url) {
+            let user = firebase.auth().currentUser;
+            if (user) {
+                let yesno = confirm("この画像を削除しますか");
+                if (yesno) {
+                    await firebase.database().ref("/contests/aplo2022/storage/" + key).remove();
+                    await firebase.storage().refFromURL(url).delete();
+                }
+            }
+        },
+        async editDB(key, data) {
+            let user = firebase.auth().currentUser;
+            if (user) {
+                await firebase.database().ref("/contests/aplo2022/storage/" + key).update(data);
+            }
+        }
+    }
+})
+
+app.use(VueEasyLightbox)
+app.mount('#app')
