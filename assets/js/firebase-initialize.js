2745fd963a83d1337ad632d6ce0aecb3818bc1de
fulfom <la.fulfom@gmail.com>
2022-12-15 21:39:23 +0900

    試し いったん戻す

--- assets/js/firebase-initialize.js
+++ assets/js/firebase-initialize.js
@@ -2,7 +2,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
     apiKey: "AIzaSyB3UbePjjc0Q5-G6uYnWntG9mdKQBL9d6w",
-    authDomain: process.env.HUGO_AUTH_DOMAIN,
+    authDomain: "auth.iolingjapan.org",
     databaseURL: "https://application-ef043.firebaseio.com",
     projectId: "application-ef043",
     storageBucket: "application-ef043.appspot.com",
