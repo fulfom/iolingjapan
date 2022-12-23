8948e3f3f48fc3fba1b10d67423433d3c5dc509b
fulfom <la.fulfom@gmail.com>
2022-12-23 18:43:11 +0900

    別のログイン方法を追加

--- assets/js/firebase-initialize.ts
+++ assets/js/firebase-initialize.ts
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { initializeAnalytics } from "firebase/analytics";
 import { browserLocalPersistence, browserSessionPersistence, EmailAuthProvider, GoogleAuthProvider, indexedDBLocalPersistence, initializeAuth, onAuthStateChanged } from "firebase/auth"
 import { getDatabase } from "firebase/database";
-import * as firebaseui from 'firebaseui';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -55,55 +54,4 @@ export const logout = () => {
                 console.log(`ログアウト時にエラーが発生しました (${error})`);
             });
     });
-}
-
-// console.log(document.getElementById("firebaseui-auth-container"), document.getElementById("firebaseui-auth-container") !== null)
-if (document.getElementById("firebaseui-auth-container") !== null) {
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            location.href = "/account/"
-        }
-        else {
-            document.getElementsByTagName('body')!.item(0)!.style.opacity = "1";
-        }
-    });
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(auth);
-
-    var uiConfig = {
-        callbacks: {
-            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-
-                // User successfully signed in.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
-
-                return true;
-            },
-            uiShown: function () {
-                // The widget is rendered.
-                // Hide the loader.
-                document.getElementById('loader')!.style.display = 'none';
-            }
-        },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'redirect',
-        signInSuccessUrl: '/account/',
-        signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            GoogleAuthProvider.PROVIDER_ID,
-            {
-                provider: EmailAuthProvider.PROVIDER_ID,
-                signInMethod: EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-                requireDisplayName: false
-            }
-        ],
-        // Terms of service url.
-        tosUrl: '/term-of-service/',
-        // Privacy policy url.
-        privacyPolicyUrl: '/privacy-policy/'
-    };
-
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
 }
